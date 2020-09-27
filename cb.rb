ebd98439473d3175e7bd9068ee0eff6435562e69
robin15 <komiyama.tomoaki@gmail.com>
2020-09-27 20:00:50 +0900

    second commit

--- cb.rb
+++ cb.rb
@@ -1,6 +1,10 @@
 #need to execute "gem install win32-clipboard" in advance.
 
 require 'win32/clipboard'
+
+ENV['PATH'] = 'C:\Ruby23-x64\work\clipboard\test'
+p path = ENV['PATH']
+
 p cp = Win32::Clipboard.data # クリップボードのデータを取得 
 
 if cp.include?("aaaaa")
@@ -14,8 +18,11 @@ else
   return
 end
 
-p cmd = "p4v -p" + server + ":" + port + "-s " + cp
+system("echo \%PATH\%")
+p cmd = "p4v -p " + server + ":" + port + "-s " + cp
 
-system(cmd)
+if system('start test.html') == nil
+    puts "error command."
+end
 
 #aaaaaa
